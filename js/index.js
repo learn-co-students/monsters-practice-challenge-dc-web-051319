a51e33ceba4c3f8f19e200e01050f866dbd563db
Matt Williams <matt.seongjun@gmail.com>
2019-06-27 13:11:34 -0400

    Finish nav buttons

---
+++ js/index.js
@@ -0,0 +1,66 @@
+function fetchMonsterList(limit, page) {
+    fetch(`http://localhost:3000/monsters?_limit=${limit}&_page=${page}`)
+        .then(response => response.json())
+        .then(monsters => {
+            renderMonsterList(monsters);
+        });
+};
+
+function renderMonsterList(monsters) {
+    let monstersContainer = document.querySelector("#monster-container");
+
+    for (let monster of monsters) {
+        let monsterContainer = document.createElement("div");
+        monsterContainer.id = `monster-${monster.id}`;
+
+        let monsterName = document.createElement("h2");
+        monsterName.innerText = monster.name;
+        monsterContainer.appendChild(monsterName);
+
+        let monsterAge = document.createElement("h3");
+        monsterAge.innerText = monster.age;
+        monsterContainer.appendChild(monsterAge);
+
+        let monsterDescription = document.createElement("p");
+        monsterDescription.innerText = monster.description;
+        monsterContainer.appendChild(monsterDescription);
+
+        monstersContainer.appendChild(monsterContainer);
+    };
+};
+
+function clearMonsterList() {
+    let monstersContainer = document.querySelector("#monster-container");
+    while (monstersContainer.firstChild) {
+        monstersContainer.removeChild(monstersContainer.firstChild);
+    }
+};
+
+function createNavEvents() {
+    let backButton = document.querySelector("#back");
+    let forwardButton = document.querySelector("#forward");
+
+    backButton.addEventListener("click", (event) => {
+        if (currentPage > 0) {
+            currentPage -= 1;
+        };
+        clearMonsterList();
+        fetchMonsterList(limit, currentPage);
+    });
+    
+    forwardButton.addEventListener("click", (event) => {
+        if (true) {
+            currentPage += 1;
+        };
+        clearMonsterList();
+        fetchMonsterList(limit, currentPage);
+    });
+};
+
+const limit = 50;
+let currentPage = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchMonsterList();
+    createNavEvents();
+});
